2d3718516e8b035a523dd3e2bcda25f3cf0cd724
Antonio Gutierrez <augutierrez@dons.usfca.edu>
2019-11-02 11:28:50 -0700

    "commit"

--- Project/src/Driver.java
+++ Project/src/Driver.java
@@ -26,89 +26,36 @@ public class Driver {
 		ArgumentParser parser = new ArgumentParser();
 		parser.parse(args);
 		InvertedIndex index = new InvertedIndex();
-		ThreadSafeInvertedIndex threadIndex = new ThreadSafeInvertedIndex();
-//		if (parser.hasFlag("-threads")) {
-//			index = new ThreadSafeInvertedIndex();
-//		}
 
-		QueryBuilder queryBuilder;
+		QueryBuilder queryBuilder = new QueryBuilder(index);
 
-		if (parser.hasFlag("-threads")) {
-			int numThreads;
-			try {
-				numThreads = Integer.parseInt(parser.getString("-threads", "5"));
-			} catch (java.lang.NumberFormatException e) {
-				numThreads = 0;
-			}
-
-			// ThreadSafeInvertedIndex threadIndex = new ThreadSafeInvertedIndex();
-			if (parser.hasFlag("-path")) {
-				Path path = parser.getPath("-path");
-				try {
-					ThreadIndexBuilder.directoryBuilder(path, threadIndex, numThreads);
-				} catch (Exception e) {
-					System.err.println("Invalid path sent to Inverted Index, unable to add :" + path
-							+ " to data structure. Please enter existing paths to textfiles.");
-				}
-			}
-			queryBuilder = new QueryBuilder(threadIndex);
-		}
-
-		// if we want it multithreaded, the workqueue must somehow take over driver I
-		// think, then create an inverted index biulder that utilizes multi threading.
-		// we need to make one thread handle one file, we can use the recursion to get
-		// the files
-		// then a thread that is ready will pick up a file as we find them.
-		// then we need those threads to use the read and write block to make sure they
-		// only write to the
-		// iverted index once at a time.
-		else {
-			if (parser.hasFlag("-path")) {
+		if (parser.hasFlag("-path")) {
 			Path path = parser.getPath("-path");
-				try {
-					InvertedIndexBuilder.directoryIterator(path, index);
-				} catch (Exception e) {
-					System.err.println("Invalid path sent to Inverted Index, unable to add :" + path
-							+ " to data structure. Please enter existing paths to textfiles.");
-				}
+			try {
+				InvertedIndexBuilder.directoryIterator(path, index);
+			} catch (Exception e) {
+				System.err.println("Invalid path sent to Inverted Index, unable to add :" + path
+						+ " to data structure. Please enter existing paths to textfiles.");
 			}
-			queryBuilder = new QueryBuilder(index);
 		}
+
 		if (parser.hasFlag("-index")) {
 			Path indexPath = parser.getPath("-index", Path.of("index.json"));
-			if (parser.hasFlag("-threads")) {
-				try {
-					threadIndex.indexWriter(indexPath);
-				} catch (Exception e) {
-					System.err.println("Invalid output file sent to indexWriter: " + indexPath
-							+ ". Please enter a valid output file path name.");
-				}
-			} else {
 			try {
 					index.indexWriter(indexPath);
-				} catch (Exception e) {
+			} catch (Exception e) {
 					System.err.println("Invalid output file sent to indexWriter: " + indexPath
 							+ ". Please enter a valid output file path name.");
-				}
 			}
 		}
 		
 		if (parser.hasFlag("-counts")) {
 			Path countsPath = parser.getPath("-counts", Path.of("counts.json"));
-			if (parser.hasFlag("-threads")) {
-				try {
-					threadIndex.countsWriter(countsPath);
-				} catch (Exception e) {
-					System.err.println("Invalid output file sent to indexWriter: " + countsPath
-							+ ". Please enter a valid output file path name.");
-				}
-			} else {
 			try {
 					index.countsWriter(countsPath);
-				} catch (Exception e) {
+			} catch (Exception e) {
 					System.err.println("Unalbe to write word counts to the path:" + countsPath
 							+ ". Please enter a valid output file path name.");
-				}
 			}
 		}
 
@@ -135,16 +82,10 @@ public class Driver {
 				System.err.println("Unable to write results into path : " + resultsPath
 						+ ". Please enter a valid output path name.");
 			}
-
 		}
-
-
-
 
 		Duration elapsed = Duration.between(start, Instant.now());
 		double seconds = (double) elapsed.toMillis() / Duration.ofSeconds(1).toMillis();
 		System.out.printf("Elapsed: %f seconds%n", seconds);
-
 	}
-
 }
